e846c0f174eef06eb1bb89fc50ca69b215e91f05
Abdelkebir Achraf <104693339+aachraf94@users.noreply.github.com>
2024-02-08 12:04:04 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "stdlib.h"
+#include <stdio.h>
+#include "shared_memory.h"
+#include "semaphore.h"
+#include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+
+int main()
+{
+    // CREATION DES SEMAPHORES
+
+    // CREATION DU SEGMENT MEMOIRE
+
+    // ATTACHEMENT DU SEGMENT MEMOIRE
+
+    // INITIALISATION DU SEGMENT MEMOIRE
+
+    // INITIALISATION DES SEMAPHORES
+
+    // CREATION DES CLIENTS
+
+    // DETACHEMENT ET SUPRESSION DES SEMAPHORES ET DU SEGMENNT
+
+    return 0;
+}
